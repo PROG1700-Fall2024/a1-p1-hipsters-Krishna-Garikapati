3e3a1b83f03787696aadbffe63c645a59aab2215
krishna-garikapati <w0502117@campus.nscc.ca>
2024-10-01 16:13:05 -0300

    program 1-assignment 1

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -1,16 +1,38 @@
+"""
+Student name:Krishna Priyanka Garikapati
+Student Number:W0502117
+Assignment # 1
+"""
 #Program 1 – Hipster's Local Vinyl Records
 #Hipster’s Local Vinyl Records sell and hand-deliver vinyl records to their customers.
 
+
+######Pseudocode########
+#input the customer name and assign it to a variable.
+#input the distance in kms and assign it to a variable. The output of input command is always string so change the datatype using float.
+#input the cost of records purchased and assign it to a variable. The output of input command is always string so change the datatype of assigned variable using float.
+#As delivery charge is contant assign valur to variable.
+#Total delivery charge=input of distance in kms * deivery charge
+#print delivery charge of customer for the distance in kms.
+#sales tax=(14/100)*cost of records purchased
+#Total purchase cost=Sales tax + cost of records purchased.
+#print the total purchase cost
+#total cost=total purchase cost + total deivery charge
+#print the total cost
+
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
+    customername=input("Enter customer name:")
+    Distance=float(input("Enter the distance for destination in km:"))
+    costofrecords=float(input("Enter cost of records purchased: $"))
+    deliveryCharge=15
+    TotalDelCharge=Distance*deliveryCharge
+    print("Purchase summary for", customername, "is")
+    print("Deivery cost $", TotalDelCharge)
+    Salestax=(14/100)*costofrecords
+    purchasecost=costofrecords+Salestax
+    totalpurchasecost=round(purchasecost,2)
+    print("Purchase cost $", totalpurchasecost)
+    Totalcost=(totalpurchasecost+TotalDelCharge)
+    print("Total cost $",Totalcost)
 
 main()
